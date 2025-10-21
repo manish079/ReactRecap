58293fd1071d7ee46a7c0d3aca5e45e6f4e9e0f7
manish079 <manishprajapat492@gmail.com>
2025-10-21 10:40:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const parent = React.createElement(
+  "div",
+  {},
+  React.createElement("h1", {}, "Happy Diwali 🪔")
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
